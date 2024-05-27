89cfa04211dca27621e7f712b9b6f6f74745a48e
sethcrist <sethcrist50@gmail.com>
2024-05-26 17:15:19 -0700

    added another useState to save the color

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -3,25 +3,44 @@ import React, { useState } from "react"
 const ColorPicker = () => {
 
   const [color, setColor] = useState("#FFFFFF")
-  const [savedColor, setSavedColor] = useState([])
+  const [savedColors, setSavedColors] = useState([])
 
   const handleColorChange = (e) => {
     setColor(e.target.value)
   }
 
+  const handleSavedColor = () => {
+    setSavedColors([...savedColors, color])
+  }
+
   return (
     <div className="page-container">
-      <div className="color-picker-container">
-        <h1 style={{ color: color, textShadow: "1px 1px 2px black" }}>Color Picker</h1>
-        <div className="color-display"
-          style={{ backgroundColor: color }}
-        >
-          <p>Color: {color}</p>
+      <div className="grid-container">
+        <div className="color-picker-container">
+          <h1 style={{ color: color, textShadow: "1px 1px 2px black" }}>Color Picker</h1>
+          <div className="color-display"
+            style={{ backgroundColor: color }}
+          >
+            <p>Color: {color}</p>
+          </div>
+          <label>Choose a Color:</label>
+          <br />
+          <input type="color" value={color} onChange={handleColorChange}></input>
+          <br />
+          <button onClick={handleSavedColor}>Save Your Color</button>
         </div>
-        <label>Choose a Color:</label>
-        <input type="color" value={color} onChange={handleColorChange}></input>
+
+        <div className="saved-color-container">
+          <h2>Saved Colors:</h2>
+          <ul>
+            {savedColors.map((savedColor, index) => (
+              <li key={index}>
+                <div className="saved-color" style={{ backgroundColor: savedColor }}>Color: {savedColor}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <p>Saved Colors:</p>
     </div>
   )
 }
